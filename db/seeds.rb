59754fff8b3b493d1fa2c7c631cb6a44f3ac79f1
Sascha Montero <saschamontero@gmail.com>
2020-09-10 08:58:49 +0200

    exercise not finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+puts "Destroying DB"
+Restaurant.destroy_all
+puts "Generating restaurants"
+10.times do |restaurant|
+  restaurant = Restaurant.create(
+    name: Faker::Name.name
+    address: Faker::Address.street_address
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164
+    category:
+
+end
